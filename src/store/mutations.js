5de380712d686ee82bca926b855fac2a249eff1d
Titouan Mathis <titouan.mathis@gmail.com>
2017-02-01 23:04:05 +0100

    Add comments to the mutations and remove the `console.log()`

--- src/store/mutations.js
+++ src/store/mutations.js
@@ -4,22 +4,47 @@
 
 import Vue from 'vue'
 
+
+
+/**
+ * Save the view width global variable
+ * @param  {Object} state The store's state object
+ * @param  {Number} value The width of the viewport
+ * @return {void}
+ */
 export const SET_VIEW_WIDTH = (state, value) => {
-	console.log('SET_VIEW_WIDTH', value)
 	Vue.set(state, 'VIEW_WIDTH', value)
 }
 
+
+/**
+ * Save the view height global variable
+ * @param  {Object} state The store's state object
+ * @param  {Number} value The height of the viewport
+ * @return {void}
+ */
 export const SET_VIEW_HEIGHT = (state, value) => {
-	console.log('SET_VIEW_HEIGHT', value)
 	Vue.set(state, 'VIEW_HEIGHT', value)
 }
 
+
+/**
+ * Save the pointer X position
+ * @param  {Object} state The store's state object
+ * @param  {Number} value The current X pointer's position
+ * @return {void}
+ */
 export const SET_POINTER_X = (state, value) => {
-	console.log('SET_POINTER_X', value)
-	Vue.set(state, 'SET_POINTER_X', value)
+	Vue.set(state, 'POINTER_X', value)
 }
 
+
+/**
+ * Save the pointer Y position
+ * @param  {Object} state The store's state object
+ * @param  {Number} value The current Y pointer's position
+ * @return {void}
+ */
 export const SET_POINTER_Y = (state, value) => {
-	console.log('SET_POINTER_Y', value)
-	Vue.set(state, 'SET_POINTER_Y', value)
+	Vue.set(state, 'POINTER_Y', value)
 }
